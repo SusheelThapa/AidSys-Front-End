bc5695190593dbf672c305536a5a0179570625af
Susheel Thapa <susheel.thapa@outlook.com>
2022-12-29 22:12:46 +0545

    Rendering the Landing page or Login page based on token present
    We have use useState to store the retrieve token
    We have use useEffect to get and store the token in useState variable
    
    Then, we have used conditional rendering to render page dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import React, { useState, useEffect } from "react";
+
 import Header from "./components/Header";
+import Login from "./components/Login";
 import Sidebar from "./components/Sidebar";
 import Workspace from "./components/Workspace";
 
+import { getToken } from "./services/token";
+
 function App() {
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    /**
+     * This useEffect get the token store in local storage and
+     * set the value of token
+     */
+    const tokenLocal = getToken();
+
+    setToken(tokenLocal);
+  }, []);
+
   return (
-    <div>
-      <Header />
-      <Sidebar />
-      <Workspace />
-    </div>
+    /**
+     * Token can be considered as identity of user.
+     * If token is present in localStorage then we will show Header SideBar Workspace
+     * If token is not present in localStorage then we will show Login page
+     */
+    <React.Fragment>
+      {token != null && (
+        <div>
+          <Header />
+          <Sidebar />
+          <Workspace />
+        </div>
+      )}
+
+      {token == null && <Login />}
+    </React.Fragment>
   );
 }
 
