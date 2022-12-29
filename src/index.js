3593eead8217b839ad5d670c5ddf0fc97b99d7c6
Susheel Thapa <susheel.thapa@outlook.com>
2022-12-29 20:44:54 +0545

    Reset margin padding of html element to 0
    index.css file is imported in index.js. Also we have set the bor-sizing to border box and some font also

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import "./index.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
