9200740c04ded45beeff0ea941b5cb29dd7ad23e
Susheel Thapa <susheel.thapa@outlook.com>
2022-12-31 09:18:29 +0545

    Render the nav items using for loop
    Created a state variable to store all the navitems
    Then, we have render those navitem store in variable using Array.map()

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,64 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Header() {
+  const [navItems] = useState([
+    "All items",
+    "Collections",
+    "Locations",
+    "Reminders",
+    "Bookings",
+    "Devices",
+    "Reports",
+    "Users",
+  ]);
+
   return (
-      <div>
-        <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary">
-          <div className="navbar container-fluid">
-            <div
-              className="navbar collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav mx-auto mb-2 mb-lg-0 ">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    All Items
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Collections
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Locations
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Reminders
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Bookings
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Issues
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Devices
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Reports
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Users
-                  </a>
-                </li>
-              </ul>
-            </div>
+    <div>
+      <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary">
+        <div className="navbar container-fluid">
+          <div
+            className="navbar collapse navbar-collapse"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0 ">
+              {/* 
+              <==== IMPORTANT =====>
+              * The below code is necessary as in future we are going to apply those is the
+              * tab the is current active
+
+
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  All Items
+                </a>
+              </li> 
+              */}
+
+              {navItems.map((item) => {
+                return (
+                  <li key={item} className="nav-item">
+                    <a href="#" className="nav-link">
+                      {item}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-        </nav>
-      </div>
-  )
+        </div>
+      </nav>
+    </div>
+  );
 }
