90a1ec8385b2ba1ec389400ec2f731a3da67eb33
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-23 14:28:17 +0545

    Added header in assets page

--- src/components/Assets.jsx
+++ src/components/Assets.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
 const Asset = () => {
-  return <h1>Asset</h1>;
+  return (
+    <div>
+      <Header />
+      <h1>Other goes here</h1>
+    </div>
+  );
 };
 
 export default Asset;
