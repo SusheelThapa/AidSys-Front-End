455255eb96e4dc6fc49a4f60ef07c96d2d89b264
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-24 01:44:54 +0545

    Change functional to class based component

--- src/components/Assets.jsx
+++ src/components/Assets.jsx
@@ -1,52 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./common/Header";
-import { getStudentDetail, getTokenData } from "../services/request";
-import { readToken } from "../services/token";
 
-const Asset = () => {
-  getTokenData(readToken()).then((studentDetail) => {
-    getStudentDetail(studentDetail.data.studentID).then((res) => {
-      console.log(res);
-    });
-  });
-  return (
-    <div>
-      <Header />
-      {/* global container */}
-      <div className="h-screen bg-gray-200">
-      {/* page one */}
-        <div className="flex space-x-6 xl:space-x-32">
-        {/* left container */}
-          <div className="flex mt-56 pt-8 pb-16 flex-col items-center bg-white w-1/2 xl:mx-14">
-            <h1 className="text-3xl font-bold font-serif text-assets-100">
-              WELCOME TO ASSETS PAGE
-            </h1>
-            <p className="text-left mx-4 text-sm font-bold mt-6 mb-10">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequatur quis, doloremque repudiandae maxime et minima aliquam
-              aperiam nostrum minus quo beatae eius sapiente ipsum sed veritatis
-              voluptatum sint voluptate ad?
-            </p>
-            <button className="bg-assets-200 text-white font-bold text-2xl py-2 px-4 rounded-lg hover:bg-indigo-500 duration-200">BOOK SEATS NOW</button>
-          </div>
-          {/* right container */}
-          <div className="w-1/2 pr-4 xl:pr-14">
-            <div className="flex  mt-4">
-              <input className="focus:outline-none rounded-lg w-2/3" type="text" name="" id="" />
-              <button className="w-1/3 bg-assets-200 mx-2 px-4 py-2 text-white font-bold text-xl rounded-lg hover:bg-indigo-500 duration-200">Search</button>
+class Asset extends Component {
+  render() {
+    return this.state.student ? (
+      <div>
+        <Header />
+        <div className="h-screen bg-gray-200">
+          {/* page one */}
+          <div className="flex space-x-6 xl:space-x-32">
+            {/* left container */}
+            <div className="flex mt-56 pt-8 pb-16 flex-col items-center bg-white w-1/2 xl:mx-14">
+              <h1 className="text-3xl font-bold font-serif text-assets-100">
+                WELCOME TO ASSETS PAGE
+              </h1>
+              <p className="text-left mx-4 text-sm font-bold mt-6 mb-10">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequatur quis, doloremque repudiandae maxime et minima
+                aliquam aperiam nostrum minus quo beatae eius sapiente ipsum sed
+                veritatis voluptatum sint voluptate ad?
+              </p>
+              <button className="bg-assets-200 text-white font-bold text-2xl py-2 px-4 rounded-lg hover:bg-indigo-500 duration-200">
+                BOOK SEATS NOW
+              </button>
             </div>
-            <div className=" bg-white mt-4 rounded-lg">
-              <h1 className="p-4 text-assets-100 text-2xl font-serif font-bold">YOUR BOOKED ASSETS</h1>
-              <p>Images goes here</p>
+            {/* right container */}
+            <div className="w-1/2 pr-4 xl:pr-14">
+              <div className="flex  mt-4">
+                <input
+                  className="focus:outline-none rounded-lg w-2/3"
+                  type="text"
+                  name=""
+                  id=""
+                />
+                <button className="w-1/3 bg-assets-200 mx-2 px-4 py-2 text-white font-bold text-xl rounded-lg hover:bg-indigo-500 duration-200">
+                  Search
+                </button>
+              </div>
+              <div className=" bg-white mt-4 rounded-lg">
+                <h1 className="p-4 text-assets-100 text-2xl font-serif font-bold">
+                  YOUR BOOKED ASSETS
+                </h1>
+                <p>Images goes here</p>
+              </div>
             </div>
           </div>
         </div>
+        <div>part 2</div>
       </div>
-      <div>
-        part 2
-      </div>
-    </div>
-  );
-};
+    ) : (
+      <h1>Loading</h1>
+    );
+  }
+}
 
 export default Asset;
