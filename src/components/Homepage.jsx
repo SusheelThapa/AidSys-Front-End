228223bb5e51a5fa19903790ddecd99ba1fcc3c9
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-23 13:13:07 +0545

    Used header component in Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 // import Workspace from "./Workspace";
 
 import Student from "../assets/img/student.png";
+import Header from "./Header";
 const Homepage = () => {
   return (
     <div id="homepage" className="flex flex-col justify-between h-screen">
@@ -16,35 +17,8 @@ const Homepage = () => {
       {/**
        * Your landing page goes here
        */}
-      <div className="flex bg-blue-800 h-20">
-        <p className="mx-8 font-bold text-white bg-blue-800 font-serif text-2xl mt-4">
-          AidSys
-        </p>
-        <div className="flex space-x-10 mx-auto bg-blue-800 items-center">
-          <button className="bg-blue-800 text-yellow-400 hover:font-bold hover:text-yellow-600">
-            Home
-          </button>
-          <button className="bg-blue-800 text-yellow-400 hover:font-bold hover:text-yellow-600">
-            What we offer
-          </button>
-          <button className="bg-blue-800 text-yellow-400 hover:font-bold hover:text-yellow-600">
-            Notice Board
-          </button>
-          <button className="bg-blue-800 text-yellow-400 hover:font-bold hover:text-yellow-600">
-            Anything new?
-          </button>
-        </div>
-        <div className="bg-blue-800 my-4 mx-8">
-          <button className="bg-transparent py-1 px-4 border text-yellow-400 hover:font-bold">
-            <Link
-              to={"/login"}
-              className="bg-transparent hover:text-yellow-500"
-            >
-              Login
-            </Link>
-          </button>
-        </div>
-      </div>
+      <Header />
+
       <div className="flex flex-col items-center justify-center">
         <div className="mt-16">
           <img className="w-80" src={Student} alt="Student" />
