d0c5a3c3794797be827639204c2cc4575b3a46e4
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-23 13:16:59 +0545

    Used Hero component in Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,38 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// import Header from "./Header";
-// import Sidebar from "./Sidebar";
-// import Workspace from "./Workspace";
-
-import Student from "../assets/img/student.png";
 import Header from "./Header";
+import Hero from "./Hero";
+
 const Homepage = () => {
   return (
     <div id="homepage" className="flex flex-col justify-between h-screen">
-      {/* <Header />
-      <Sidebar />
-      <Workspace /> */}
+      <Header />
+      <Hero />
 
       {/**
-       * Your landing page goes here
+       * TODO: Refactor below content into some components
        */}
-      <Header />
-
-      <div className="flex flex-col items-center justify-center">
-        <div className="mt-16">
-          <img className="w-80" src={Student} alt="Student" />
-        </div>
-
-        <input
-          className="w-96 bg-white rounded-lg shadow-lg p-2 focus:outline-none"
-          type="text"
-          placeholder="Search for anything here"
-        />
-        <button className="py-2 px-4 mt-4 bg-yellow-500 shadow-lg border border-yellow-800 hover:bg-yellow-600 font-bold">
-          SEARCH
-        </button>
-      </div>
       <div className="flex justify-center">
         <div className="flex flex-col items-center bg-blue-800 mt-20 rounded-t-full w-[700px] h-28 xl:w-[1000px]">
           <Link
