4302fc6fd32ffeb476d071c0b642f46907fb0158
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-01-24 09:00:11 +0545

    Added loading page

---
+++ src/components/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/css/loading.css"
+const loading = () => {
+  return (
+    <div class="loading-container">
+      <div class="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default loading;
