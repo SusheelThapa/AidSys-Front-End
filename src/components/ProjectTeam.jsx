00577058d644ad299eb27d849acf77d65f36e6bb
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-26 02:43:08 +0545

    Customize with CSS to look better

--- src/components/ProjectTeam.jsx
+++ src/components/ProjectTeam.jsx
@@ -1,18 +1,30 @@
 import React from "react";
-import RoundPerson from "../assets/img/round-person.png"
-function ProjectTeam() {
+import RoundPerson from "../assets/img/round-person.png";
+function ProjectTeam({ teamMember }) {
+  const handleName = (link) => {
+    window.location.href = link;
+  };
   return (
     <div className="mt-20 mx-10">
       <h1 className="text-3xl text-assets-100 font-bold  font-serif">
         Current Team
       </h1>
-      <div className="flex mx-auto max-w-fit rounded-xl shadow-xl py-4 px-16 border mt-8">
-        <img className="w-44" src={RoundPerson} alt="" />
-        <div className="flex flex-col justify-center ml-8">
-            <h1 className="text-2xl text-assets-100">Person Name</h1>
-            <h3 className="mt-2 text-xl text-gray-600">Project Name</h3>
-            <p className="mt-2 italic text-lg mx-8">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, mollitia quibusdam voluptate ipsum debitis id enim repellendus soluta sed veniam."</p>
-        </div>
+      <div className="flex justify-center m-3">
+        {teamMember.map((member) => {
+          return (
+            <div className="flex m-3 max-w-fit rounded-xl shadow-xl py-4 px-16 border mt-8">
+              <img className="w-44" src={RoundPerson} alt="" />
+              <div className="flex flex-row justify-center items-center ml-8">
+                <h1
+                  className="text-2xl text-assets-100"
+                  onClick={() => handleName(`${member.link}`)}
+                >
+                  {member.name}
+                </h1>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
