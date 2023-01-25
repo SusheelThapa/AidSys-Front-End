8a1ed2735ef58ffc70e37040d0092a2e75c52c7f
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-25 21:59:15 +0545

    Created new component and set up route for that component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./common/Header";
+
+const Profile = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <h1>Other code goes here</h1>
+    </React.Fragment>
+  );
+};
+
+export default Profile;
