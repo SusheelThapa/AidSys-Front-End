70a6a5e83c0b56be35d9f396ad62aed89db0b38b
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-25 21:10:18 +0545

    Setup environment for individual project details

---
+++ src/components/Project.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Header from "./common/Header";
+
+import { getStudentDetail, getTokenData } from "../services/request";
+import { doesTokenExist, readToken } from "../services/token";
+import Loading from "./Loading";
+
+class Project extends Component {
+  state = { student: null };
+
+  componentDidMount() {
+    if (doesTokenExist()) {
+      getTokenData(readToken()).then((res) => {
+        getStudentDetail(res.data.studentID).then((studentdata) => {
+          if (studentdata.success) {
+            const student = studentdata.student[0];
+            console.log(student);
+            this.setState({ student: student });
+          } else {
+            alert(studentdata.message);
+          }
+        });
+      });
+    } else {
+      /**
+       * TODO: Redirect to login page
+       */
+    }
+  }
+  render() {
+    return this.state.student ? (
+      <React.Fragment>
+        <Header studentName={this.state.student.username.toUpperCase()} />
+        <h1>Other Code for project goes here</h1>
+      </React.Fragment>
+    ) : (
+      <Loading />
+    );
+  }
+}
+
+export default Project;
