6276babb64a1557fd9fc3f317ad3c0a30e9b9183
mhrzneeka <mhrzneeka@gmail.com>
2023-01-25 23:30:42 +0545

    profile updated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,12 @@ const Profile = () => {
   return (
     <React.Fragment>
       <Header />
-      <h1>Other code goes here</h1>
+      <div className="background-image">
+        <img id='graduate' src='https://cdn4.iconfinder.com/data/icons/education-business-part-1/513/1-512.png'></img>
+        {/* <img src="E:\Locus\AidSys-Front-End\src\assets\img\classroompic.jpg"></img> */}
+      <button type="button-y" class="btn btn-warning">EXPLORE NOW</button>
+      <div className="motto">Our Motto is.....</div>
+      </div>
     </React.Fragment>
   );
 };
