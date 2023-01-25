a280ea4042a2266bef3d635449c62c6e746521f8
Parikshit Adhikari <parikshitadhikari@gmail.com>
2023-01-25 23:00:17 +0545

    added ProjectApplication.jsx and footer

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
 import React, { Component, useState } from "react";
-// import Header from "./common/Header";
+import Header from "./common/Header";
 
 // import { getStudentDetail, getTokenData } from "../services/request";
 // import { doesTokenExist, readToken } from "../services/token";
@@ -7,9 +7,14 @@ import React, { Component, useState } from "react";
 import Laptop from "../assets/img/laptop.png";
 import LaptopTwo from "../assets/img/laptop2.png";
 import LaptopThree from "../assets/img/laptop3.png";
-import { BsChevronCompactLeft, BsChevronCompactRight, BsFileSlides } from "react-icons/bs";
+import {
+  BsChevronCompactLeft,
+  BsChevronCompactRight,
+} from "react-icons/bs";
 import ProjectTeam from "./ProjectTeam";
 import ProjectVacancy from "./ProjectVacancy";
+import ProjectApplication from "./ProjectApplication";
+import Footer from "./common/Footer";
 // class Project extends Component {
 //   state = { student: null };
 
@@ -57,21 +62,24 @@ const Project = () => {
     },
   ];
 
-  const [currentIndex, setCurrentIndex]=useState(0)
-  const prevSlide = ()=>{
-    const isFirstSlide = currentIndex ===0;
-    const newIndex = isFirstSlide?slider.length-1:currentIndex-1;
-    setCurrentIndex(newIndex)
-  }
-  const nextSlide=()=>{
-    const isLastSlide = currentIndex === slider.length-1;
-    const newIndex = isLastSlide?0:currentIndex+1
-    setCurrentIndex(newIndex)
-  }
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slider.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slider.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
   return (
     <>
+      <Header />
       <div className="mx-10 my-12">
-        <h1 className="text-3xl text-assets-100 font-bold  font-serif">Project Name</h1>
+        <h1 className="text-3xl text-assets-100 font-bold  font-serif">
+          Project Name
+        </h1>
         <div className="flex space-x-10 2xl:space-x-48">
           <div className="mt-4 w-3/5">
             <p className="text-lg mt-4">
@@ -103,20 +111,28 @@ const Project = () => {
           <div className="w-2/5 flex-col flex justify-center">
             <div className="max-w-[600px] relative group rounded-xl">
               <img src={slider[currentIndex].image} alt="" />
-            {/* left arrow */}
-            <div className="absolute top-[40%] bg-white bg-opacity-50 mx-3 cursor-pointer rounded-full -translate-x-0 translate-y-[50] hidden group-hover:block" onClick={prevSlide}>
-              <BsChevronCompactLeft size={60} />
-            </div>
-            <div className="absolute top-[40%] bg-white bg-opacity-50 cursor-pointer rounded-full -translate-x-0 translate-y-[50] right-5 hidden group-hover:block" onClick={nextSlide}>
-              {/* right arrow */}
-              <BsChevronCompactRight size={60} />
-            </div>
+              {/* left arrow */}
+              <div
+                className="absolute top-[40%] bg-white bg-opacity-50 mx-3 cursor-pointer rounded-full -translate-x-0 translate-y-[50] hidden group-hover:block"
+                onClick={prevSlide}
+              >
+                <BsChevronCompactLeft size={60} />
+              </div>
+              <div
+                className="absolute top-[40%] bg-white bg-opacity-50 cursor-pointer rounded-full -translate-x-0 translate-y-[50] right-5 hidden group-hover:block"
+                onClick={nextSlide}
+              >
+                {/* right arrow */}
+                <BsChevronCompactRight size={60} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <ProjectTeam/>
-      <ProjectVacancy/>
+      <ProjectTeam />
+      <ProjectVacancy />
+      <ProjectApplication />
+      <Footer />
     </>
   );
 };
