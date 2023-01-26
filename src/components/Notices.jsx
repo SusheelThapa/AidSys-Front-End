bfe63409672a2ad66ba8e4c92930a12574f6c5aa
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-27 00:11:03 +0545

    Fix prb of reading unknown property

--- src/components/Notices.jsx
+++ src/components/Notices.jsx
@@ -28,7 +28,7 @@ class Notices extends Component {
   render() {
     return this.state.student ? (
       <React.Fragment>
-        <Header studentName={this.state.student.username.toUpperCase()} />
+        <Header studentName={this.state.student.name} />
         <div className="flex mt-16">
           <div className="w-[70%] space-y-20">
             <h1 className="text-3xl mx-10 font-bold font-serif text-assets-100">
